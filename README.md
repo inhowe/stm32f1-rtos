d5048d2c469ae541048ce0271632277b1a3d660c
inhowe <inhowe@qq.com>
2019-12-02 12:43:26 +0800

    modify name

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # PLEASE GIVE ME STAR
 stm32f1系列的操作系统，请给我星星呀~
-## os - pendSV - f1
+## easyScheduler - level0 - pendSV - f1
 简单的上下文切换器
-## os - basic - f1
+## easyScheduler - level1 - os - f1
 简单的任务调度器
-## os - general - f1
+## easyScheduler - level2 - os - f1
 稍微实际点的操作系统
---
+++ RTOS-TaoBao/easyScheduler - level0 - pendSV - f1/User/peripheral.c
@@ -0,0 +1,69 @@
+/**
+  ******************************************************************************
+
+  ******************************************************************************
+  */
+  
+#include "peripheral.h"   
+
+ /**
+  * @brief  初始化控制LED的IO
+  * @param  无
+  * @retval 无
+  */
+void LED_GPIO_Config(void)
+{		
+		GPIO_InitTypeDef GPIO_InitStructure;
+		RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOB, ENABLE); 
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8|GPIO_Pin_9;	
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;   
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; 
+		GPIO_Init(GPIOB, &GPIO_InitStructure);	
+		/* 关闭所有led灯	*/
+		GPIO_SetBits(GPIOB, GPIO_Pin_8|GPIO_Pin_9);
+		 
+}
+
+void USART1_Config(void)
+{
+		GPIO_InitTypeDef GPIO_InitStructure;
+		USART_InitTypeDef USART_InitStructure;
+		
+		/* config USART1 clock */
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
+		
+		/* USART1 GPIO config */
+		/* Configure USART1 Tx (PA.09) as alternate function push-pull */
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+		GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+		/* Configure USART1 Rx (PA.10) as input floating */
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+		GPIO_Init(GPIOA, &GPIO_InitStructure);
+			
+		/* USART1 mode config */
+		USART_InitStructure.USART_BaudRate = 115200;
+		USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+		USART_InitStructure.USART_StopBits = USART_StopBits_1;
+		USART_InitStructure.USART_Parity = USART_Parity_No ;
+		USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+		USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+		USART_Init(USART1, &USART_InitStructure); 
+		USART_Cmd(USART1, ENABLE);
+}
+
+///重定向c库函数printf到USART1
+int fputc(int ch, FILE *f)
+{
+		/* 发送一个字节数据到USART1 */
+		USART_SendData(USART1, (uint8_t) ch);
+		
+		/* 等待发送完毕 */
+		while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);		
+	
+		return (ch);
+}
+/*********************************************END OF FILE**********************/
